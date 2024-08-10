ac8c8ab80ba977a1c1ebc74e4250338ba4e065ec
Yusuf Abdullahi Temidayo <yusufabdullahi2910@gmail.com>
2024-08-10 22:44:52 +0100

    feedback

---
+++ lib/feedback/home.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _FeedbackPageState createState() => _FeedbackPageState();
+}
+
+class _FeedbackPageState extends State<Home> {
+  final _formKey = GlobalKey<FormState>();
+  bool _submitted = false;
+
+  void _submitFeedback() {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        _submitted = true;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: _submitted
+            ? Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.check_circle,
+                      color: Colors.green,
+                      size: 100,
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      'Thank you for your feedback!',
+                      style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            : Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: 50),
+                  Text(
+                    'Feedback Session',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 40,
+                      fontFamily: "Signatra",
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'We value your feedback',
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 30,
+                      fontFamily: "Signatra",
+                    ),
+                  ),
+                  SizedBox(height: 50),
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'Your Feedback',
+                            border: OutlineInputBorder(),
+                          ),
+                          maxLines: 5,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your feedback';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                          },
+                        ),
+                        SizedBox(height: 20),
+                        Center(
+                          child: ElevatedButton(
+                            onPressed: _submitFeedback,
+                            child: Text('Submit'),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orange,
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 40, vertical: 15),
+                              textStyle: TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+      ),
+    );
+  }
+}
